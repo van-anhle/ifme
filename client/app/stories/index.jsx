5446e47d5175a92965339d58cd2d7ac20e6ad065
Carl <summers.carl@gmail.com>
2018-03-05 16:28:30 -0500

    Sidenav updated

--- client/app/stories/index.jsx
+++ client/app/stories/index.jsx
@@ -8,7 +8,6 @@ import ChartControl from '../bundles/momentDashboards/components/ChartControl';
 
 import Logo from '../bundles/shared/components/Logo';
 import Input from '../bundles/shared/components/Input';
-import Button from '../bundles/shared/components/Button';
 import SideNav from '../bundles/shared/components/SideNav';
 
 storiesOf('Logo', module)
@@ -63,24 +62,7 @@ storiesOf('Input', module)
     <Input dark large label="Hello" placeholder="Placeholder" />
   ));
 
-  storiesOf('Button', module)
-    .add('ButtonDefault-Small', () => (
-      <Button size="small" color="purple" title="hi"></Button>
-    ))
-    .add('ButtonSecondary-Small', () => (
-      <Button size="small" color="silver"></Button>
-    ))
-    .add('ButtonDefault-Large', () => (
-      <Button size="large" color="purple"></Button>
-    ))
-    .add('ButtonSecondary-Large', () => (
-      <Button size="large" color="silver"></Button>
-    ));
-
     storiesOf('SideNav', module)
-    .add('SideNav-Purple', () => (
-      <SideNav color="purple"></SideNav>
-    ))
-    .add('SideNav-Silver', () => (
-      <SideNav color="silver"></SideNav>
+    .add('SideNav-with-dropdown', () => (
+      <SideNav></SideNav>
     ));
